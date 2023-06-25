75bddc41c553ab7472e7757789f798e1b4632c34
megandee <123526537+megandee@users.noreply.github.com>
2023-06-25 18:56:39 -0500

    initial commit

---
+++ treasurehunt.js
@@ -0,0 +1,67 @@
+// apple is a parameter
+  // a parameter is a placeholder for the argument
+
+  
+
+  const changeToEmoji = (clickMe) => {
+    if(clickMe === "raiders") {
+      document.getElementById("raiders").innerHTML = "🏈"
+      alert("You scored a touchdown!")
+    } 
+   
+    else if(clickMe === "chiefs") {
+      document.getElementById("chiefs").innerHTML = "🏈"
+      alert("You scored a touchdown!")
+    } 
+   
+    else if(clickMe === "cowboys") {
+      document.getElementById("cowboys").innerHTML = "❌"
+      alert("You fumbled and lost 👎")
+    }
+    
+    else if(clickMe === "giants") {
+        document.getElementById("giants").innerHTML = "❌"
+        alert("You fumbled and lost 👎")
+    }
+      
+    else if(clickMe === "broncos") {
+        document.getElementById("broncos").innerHTML = "❌"
+        alert("You fumbled and lost 👎")
+    }
+      
+    else if(clickMe === "chargers") {
+        document.getElementById("chargers").innerHTML = "❌"
+        alert("You fumbled and lost 👎")
+    }
+    
+    else if(clickMe === "browns") {
+        document.getElementById("browns").innerHTML = "❌"
+        alert("You fumbled and lost 👎")
+    }
+   
+    else if(clickMe === "steelers") {
+        document.getElementById("steelers").innerHTML = "❌"
+        alert("You fumbled and lost 👎")
+    }
+    
+    else if(clickMe === "bears") {
+        document.getElementById("bears").innerHTML = "❌"
+        alert("You fumbled and lost 👎")
+    }
+    
+    else if(clickMe === "eagles") {
+        document.getElementById("eagles").innerHTML = "❌"
+        alert("You fumbled and lost 👎")
+    }
+    
+    else if(clickMe === "bengals") {
+        document.getElementById("bengals").innerHTML = "❌"
+        alert("You fumbled and lost 👎")
+    }
+     
+    else if(clickMe === "titans") {
+        document.getElementById("titans").innerHTML = "❌"
+        alert("You fumbled and lost 👎")
+    }
+  }
+
